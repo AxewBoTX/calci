7cb22971abbb8bdd008edcc160d0a7f37e50bab7
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-05-28 15:28:10 +0530

    commit

--- calci-android/lib/mainLayout.js
+++ calci-android/lib/mainLayout.js
@@ -8,8 +8,8 @@ export default function MainLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Just A Calculator" />
         <meta name="author" content="AxewBoTX" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <link rel="shortcut icon" href="/con.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="shortcut icon" href="/logo.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Calci" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@AxewBoTX" />
