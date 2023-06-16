ce0334990915d4328d090a47a96888d0caa328a0
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-06-16 06:56:48 +0530

    commit

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,26 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+import { Box } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+
+const SmallNavbar = () => {
+  return <Box>SmallNavbar</Box>;
 };
+const BigNavbar = () => {
+  return <Box>BigNavbar</Box>;
+};
+
+const Navbar = () => {
+  const [width, setWidth] = useState(null);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return <Box>{width >= 800 ? <BigNavbar /> : <SmallNavbar />}</Box>;
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "/lib/theme.js";
 import "@fontsource/space-grotesk";
+import Navbar from "/components/navbar.js";
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
