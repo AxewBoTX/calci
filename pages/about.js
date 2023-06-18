72cc6aea7971e74e31c8b7dcafdcc7f168403893
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-06-18 09:17:25 +0530

    commit

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,102 @@
 import MainLayout from "/lib/mainLayout.js";
-import { Container, Box } from "@chakra-ui/react";
+import Link from "next/link";
+import {
+  Container,
+  Box,
+  IconButton,
+  useColorModeValue,
+  Text,
+} from "@chakra-ui/react";
+import {
+  FaTwitter,
+  FaDiscord,
+  FaGlobe,
+  FaYoutube,
+  FaGithub,
+} from "react-icons/fa";
 
 const About = () => {
   return (
     <MainLayout>
-      <Container mt="170px" width="600px" textAlign="center" fontSize="20px">
-        <Box display="inline-block" fontSize="50px" lineHeight="0">
-          Calci
+      <Box
+        mt="150px"
+        width="100%"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Box display="flex" fontSize="22px" maxW="600px">
+          <Text textAlign="center">
+            <b>Calci</b>, as its name and slogan suggests, is just a calculator.
+            You neither can nor should expect anything more from it, it&apos;s
+            just a basic calculator. It don&apos;t even contain any actualy way
+            for you to square a number, you just have to figure it out yourself.
+          </Text>
         </Box>
-        as its name and slogan suggests, is just a calculator. You neither can
-        nor should expect nothing more to get from it, its just a basic
-        calculator. It don&apos;t even contain any actualy way for you to square
-        two numbers, you just have to figure it out yourself.
-      </Container>
+        <Box mt="20px" display="flex" fontSize="22px" maxW="600px">
+          <Text textAlign="center">
+            I am <b>AxewBoTX</b>, you can call me Axew. I am just a random guy
+            who loves coding and creating new things. I worked on this projet
+            just for the purpose of learning how to connect Web and Android
+            development.
+          </Text>
+        </Box>
+        <Box mt="20px" display="flex" fontSize="22px" maxW="600px">
+          <Text textAlign="center">
+            You can find me on almost all the social media platforms.
+          </Text>
+        </Box>
+        <Container
+          mt="20px"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-evenly"
+          minW="300px"
+          maxW="500px"
+          mb="80px"
+        >
+          <IconButton
+            size="lg"
+            colorScheme="teal"
+            icon={<FaGlobe />}
+            border={useColorModeValue("2px solid black", "2px solid white")}
+            as={Link}
+            href="https://axewbotx.is-a.dev"
+          />
+          <IconButton
+            size="lg"
+            colorScheme="teal"
+            icon={<FaTwitter />}
+            border={useColorModeValue("2px solid black", "2px solid white")}
+            as={Link}
+            href="https://twitter.com/axewbotx"
+          />
+          <IconButton
+            size="lg"
+            colorScheme="teal"
+            icon={<FaGithub />}
+            border={useColorModeValue("2px solid black", "2px solid white")}
+            as={Link}
+            href="https://github.com/axewbotx"
+          />
+          <IconButton
+            size="lg"
+            colorScheme="teal"
+            icon={<FaDiscord />}
+            border={useColorModeValue("2px solid black", "2px solid white")}
+            as={Link}
+            href="https://discordapp.com/users/757816994178531369"
+          />
+          <IconButton
+            size="lg"
+            colorScheme="teal"
+            icon={<FaYoutube />}
+            border={useColorModeValue("2px solid black", "2px solid white")}
+            as={Link}
+            href="https://www.youtube.com/@AxewBoTX"
+          />
+        </Container>
+      </Box>
     </MainLayout>
   );
 };
